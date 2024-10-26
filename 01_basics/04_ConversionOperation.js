25dfb927311547fec7f3f395f920ed2919a48cda
ADITI ANANT <138895705+aditianant@users.noreply.github.com>
2024-10-26 13:46:04 +0000

    Conversion Operation

---
+++ 01_basics/04_ConversionOperation.js
@@ -0,0 +1,21 @@
+let score="33"
+console.log(typeof score)
+
+let scoreInNumber=Number(score)         //The "Number" keyword confirms that the value comes in number (type conversion)
+console.log(typeof scoreInNumber)       //If there are any letters present in the string still the type will be converted
+
+let value="33abc"
+console.log(typeof value)
+
+let valueInNumber=Number(value)
+console.log(typeof valueInNumber)       //Here, the type is converted but the number comes as NaN
+console.log(valueInNumber)
+
+// If the value is digits+letters or undefined then the answer comes as NaN
+// If the value is null or false then the answer comes as 0
+// "33" => 33
+// "33abc" => NaN
+// undefined => NaN
+// null => 0
+// false => 0
+// true => 1
